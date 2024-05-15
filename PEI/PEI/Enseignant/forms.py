0fd23d93ac06e51b4066ddaa560f5eff30003c68
Cheddou <23064@esp.mr>
2024-05-15 13:44:21 +0000

    j'aoute la confirmation et la validation du donnees d'enseignants

---
+++ PEI/PEI/Enseignant/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from Enseignant.models import Category, Enseignant
+
+
+class CategoryForm(forms.ModelForm):  
+    class Meta:  
+        model = Category  
+        fields = [
+            'nom',
+            'photo',
+        ]
+        widgets = {
+            'nom': forms.TextInput(attrs={ 'class': 'form-control' }),
+            'photo': forms.FileInput(attrs={'class':'form-control'}),
+        }
+
+
+class EnseignantForm(forms.ModelForm):
+    class Meta:
+        model = Enseignant
+        fields = [
+            'name',
+            'prenom',
+            'email',
+            'contact',
+            'age',
+            'sexe',
+            'cv',
+            'photo',
+            'experience',
+            'cartier',
+            'niveau',
+            'categories',
+            'status',
+        ]
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'prenom': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'contact': forms.TextInput(attrs={'class': 'form-control'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control'}),
+            'sexe': forms.TextInput(attrs={'class': 'form-control'}),
+            'cv': forms.FileInput(attrs={'class': 'form-control'}),
+            'photo': forms.FileInput(attrs={'class': 'form-control'}),
+            'experience': forms.TextInput(attrs={'class': 'form-control'}),
+            'cartier': forms.TextInput(attrs={'class': 'form-control'}),
+            'niveau': forms.TextInput(attrs={'class': 'form-control'}),
+            'categories': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }        
