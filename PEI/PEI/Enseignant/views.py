8f9d13735d0032de0aba3b6ac74f8619da0c2112
mohamed23012 <23012@esp.mr>
2024-05-15 11:09:14 +0000

    j'ajoute la tache de recherche d'enseignant premierement je modifier le forms.py en ajoutant le forme de recherche d'enseignants aprrs j'ajoute mon vue pour le filtrer et  rendre le template recherche.html dans le recherche.html et j'extends daschboord.html le base de crud qui est fait par mon camarades

--- PEI/PEI/Enseignant/views.py
+++ PEI/PEI/Enseignant/views.py
@@ -1,3 +1,109 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from Enseignant.forms import CategoryForm, EnseignantForm
+from Enseignant.models import Category, Enseignant
+from .forms import RechercheEnseignantForm
 
 # Create your views here.
+def ajoutercategory(request):  
+    if request.method == "POST" :  
+        formcategory = CategoryForm(request.POST, request.FILES)  
+        if formcategory.is_valid():  
+             
+                formcategory.save()  
+                return redirect('list_category')
+                print("ah")  
+            
+        else :
+            print ("ah1")   
+    else:  
+        formcategory = CategoryForm() 
+        print("err")
+        
+    return render(request,'ajoutercategory.html',{'formcategory':formcategory}) 
+
+def list_category(request):  
+    categoryts = Category.objects.all()  
+    return render(request,"liste_category.html",{'categoryts':categoryts})  
+
+def modifier(request, id):  
+    category = Category.objects.get(id=id)  
+    if request.method == 'POST':
+        formcategory = CategoryForm(request.POST, instance=category)  
+        if formcategory.is_valid():  
+            formcategory.save()  
+            return redirect("list_category")  
+    else:
+        formcategory= CategoryForm(instance=category)
+        print("ah1")
+
+    return render(request, 'modifier_category.html', {'category': category, 'formcategory': formcategory})
+ 
+def suprimer(request, id):
+    category = get_object_or_404(Category, id=id)
+    return render(request, 'suprimer.html', {'category': category})
+
+     
+
+     
+def destroycategory(request, id):
+        category = Category.objects.get(id=id)  
+        category.delete()  
+        return redirect("list_category" )   
+
+
+def ajouterenseignant(request):  
+    if request.method == "POST" :  
+        form = EnseignantForm(request.POST, request.FILES)  
+        if form.is_valid():  
+             
+                form.save()  
+                return redirect('liste_enseignant')
+                print("ah")  
+            
+        else :
+            print ("ah1")   
+    else:  
+        form = EnseignantForm() 
+        print("err")
+        
+    return render(request,'ajouterenseignant.html',{'form':form}) 
+def liste_enseignant(request):  
+    enseignants = Enseignant.objects.all()  
+    return render(request,"liste_enseignants.html",{'enseignants':enseignants})   
+
+
+def update(request, id):  
+    enseignant = Enseignant.objects.get(id=id)  
+    if request.method == 'POST':
+        form = EnseignantForm(request.POST, request.FILES, instance=enseignant)  
+        if form.is_valid():  
+            form.save()  
+            return redirect("liste_enseignant")  
+    else:
+        form = EnseignantForm(instance=enseignant)
+        print("ah1")
+
+    return render(request, 'modifier_enseignant.html', {'enseignant': enseignant, 'form': form})
+
+def confirm_delete(request, id):
+    enseignant = get_object_or_404(Enseignant, id=id)
+    return render(request, 'confirme.html', {'enseignant': enseignant})
+
+     
+
+     
+def destroy(request, id):
+        enseignant = Enseignant.objects.get(id=id)  
+        enseignant.delete()  
+        return redirect("liste_enseignant" )  
+def recherche_enseignant(request):
+    form = RechercheEnseignantForm(request.GET or None)
+    enseignants = Enseignant.objects.all()
+    if form.is_valid():
+        name = form.cleaned_data.get('name')
+        if name:
+            enseignants = enseignants.filter(name__icontains=name)
+    print(enseignants)
+
+    return render(request, 'recherche.html', {'form': form, 'enseignants': enseignants})
